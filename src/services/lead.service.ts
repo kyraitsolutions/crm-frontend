468e21d0c33a183e7fa6c03b690b16016e05d041
Hem Bhadur <sushilkc2611@gmail.com>
2025-11-24 22:07:07 +0530

    fix the pagination logic and make a component for that and reslove the issue not getting data

--- src/services/lead.service.ts
+++ src/services/lead.service.ts
@@ -1,22 +1,20 @@
-
 import type { ApiResponse } from "@/types";
 import { ApiService } from "./api.service";
 
 export class LeadService extends ApiService {
-    async getLeads(
-        accountId: string,
-        params: Record<string, any>
-    ): Promise<ApiResponse<any>> {
+  async getLeads(
+    accountId: string,
+    params: Record<string, any>
+  ): Promise<ApiResponse<any>> {
+    const queryString = new URLSearchParams(
+      Object.entries(params).reduce((acc, [key, val]) => {
+        if (val !== undefined && val !== null) {
+          acc[key] = String(val);
+        }
+        return acc;
+      }, {} as Record<string, string>)
+    ).toString();
 
-        const queryString = new URLSearchParams(
-        Object.entries(params).reduce((acc, [key, val]) => {
-            if (val !== undefined && val !== null) {
-            acc[key] = String(val);
-            }
-            return acc;
-        }, {} as Record<string, string>)
-        ).toString();
-
-        return await this.get(`/account/${accountId}/leads?${queryString}`);
-    }
+    return await this.get(`/account/${accountId}/leads?${queryString}`);
+  }
 }
