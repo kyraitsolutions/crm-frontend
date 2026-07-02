0c9764f1eb73736374da31ca6c50d15777da196e
abhijeet <abhijeetsingh5631@gmail.com>
2026-07-02 10:14:19 +0530

    update

--- src/pages/LeadCentre/components/LeadTable.tsx
+++ src/pages/LeadCentre/components/LeadTable.tsx
@@ -112,8 +112,7 @@ const LeadTable = () => {
       <div className="flex h-125 w-full items-center justify-center">
         <div className="text-center">
           <img
-            src="/src/assets/nochathistoryfound_CnSlq9EOHBW59HI8RYjeSpZ_WbWyQz9RmyNcvToLwGw4g31mlf1rnCox3Y3F-6xk_.svg"
-            rel="preload"
+            src="/src/assets/converted_image_transparent.png"
             fetchPriority="high"
             alt="No chats yet"
             className="w-75"
